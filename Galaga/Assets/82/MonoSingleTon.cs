76013b4e06a63e66f90f703918e8545044790a7e
bossxz <moonkanghyuck@gmail.com>
2021-07-13 15:35:22 +0900

    20210713
    싱글톤 완전 적용

--- Galaga/Assets/82/MonoSingleTon.cs
+++ Galaga/Assets/82/MonoSingleTon.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class MonoSingleTon<T> : MonoBehaviour where T : MonoBehaviour
 {
+    private static bool shuttingDown = false;
     private static T instance;
 
     public static T Instance
     {
         get
         {
+            if (shuttingDown)
+            {
+                Debug.LogWarning("[Singleton] Instance " + typeof(T) + "returning null.");
+                return null;
+            }
             if(instance == null)
             {
                 instance = FindObjectOfType<T>();
